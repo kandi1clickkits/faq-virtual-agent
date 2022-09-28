d8330973d1d5a55d39f0171768d8b62c5f7f303d
kandi1clickkits <113971260+kandi1clickkits@users.noreply.github.com>
2022-09-28 10:49:02 +0530

    Updated files

--- kit_installer_.bat
+++ kit_installer_.bat
@@ -11,7 +11,7 @@ REM update below path if required
 SET PY_LOCATION="C:\Python"
 SET PY_VERSION=3.9.8
 SET PY_DOWNLOAD_URL=https://www.python.org/ftp/python/3.9.8/python-3.9.8-amd64.exe
-SET REPO_DOWNLOAD_URL=https://github.com/kandi1clickkits/faq-virtual-agent/releases/download/v1.0.0/faq-virtual-agent-main.zip
+SET REPO_DOWNLOAD_URL=https://github.com/kandi1clickkits/faq-virtual-agent/releases/download/v1.0.0/faq-virtual-agent.zip
 SET REPO_DEPENDENCIES_URL=https://raw.githubusercontent.com/kandi1clickkits/faq-virtual-agent/main/requirements.txt
 SET REPO_NAME=faq-virtual-agent.zip
 SET EXTRACTED_REPO_DIR=faq-virtual-agent
